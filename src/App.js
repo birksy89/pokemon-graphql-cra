ba6f68ffd84395fbc716497f5b5f9f660114e778
Andrew <andrew@purplecs.com>
2019-04-02 16:01:52 +0100

    Basic Routing Set Up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
-import PokemonList from './components/PokemonList';
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import PokemonList from "./components/PokemonList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <PokemonList/>
+          <Link to="/">Home</Link>
+          <Link to="/detail">Details</Link>
+
+          <Switch>
+            <Route exact path="/" component={PokemonList} />
+            <Route exact path="/detail" component={() => <p>Hello</p>} />
+          </Switch>
         </header>
       </div>
     );
---
+++ src/components/PokemonDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PokemonDetail extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Details...</h1>
+      </div>
+    )
+  }
+}
