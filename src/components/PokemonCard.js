18109917fd6567f0402bd2055ebffa8ea1d5cf76
Andrew <andrew@purplecs.com>
2019-04-02 16:43:35 +0100

    Linking up all pokemon in the list to respective detail page

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,22 +1,19 @@
-
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function PokemonCard(props) {
-
-    console.log(props);
+  console.log(props);
 
   return (
     <div>
-      {props.name}
+      <Link to={`/${props.name}`}>{props.name}</Link>
     </div>
-  )
+  );
 }
 
 PokemonCard.propTypes = {
-    name: PropTypes.string.isRequired
-}
+  name: PropTypes.string.isRequired
+};
 
-export default PokemonCard
-
-
+export default PokemonCard;
